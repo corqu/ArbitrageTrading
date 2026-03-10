cb59317f6f7b764caa3f5210332117eb761f33ff
Dylan <leehc12dl@naver.com>
2026-03-10 13:52:41 +0900

    BE 생성

---
+++ Backend/src/main/java/corque/gimpalarm/GimpAlarmApplication.java
@@ -0,0 +1,13 @@
+package corque.gimpalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimpAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GimpAlarmApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/corque/gimpalarm/GimpAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package corque.gimpalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimpAlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
