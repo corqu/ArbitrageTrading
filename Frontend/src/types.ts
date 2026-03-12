6319c71d47af0038b324cb6aa186f2b70944c075
Dylan <leehc12dl@naver.com>
2026-03-12 15:16:46 +0900

    프론트 수정

---
+++ Frontend/src/types.ts
@@ -0,0 +1,19 @@
+export interface KimchPremium {
+  symbol: string;
+  domesticExchange: string;
+  foreignExchange: string;
+  ratio: number;
+  fundingRate: number | null;
+  adjustedApr: number | null;
+  liquidationPrice: number | null;
+  time?: string;
+}
+
+export interface BotStatus {
+  [symbol: string]: boolean;
+}
+
+export interface TradingRequest {
+  symbol: string;
+  action: 'START' | 'STOP';
+}
